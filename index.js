99daea3b5bc6c72c96d5c1f6ad68b7337329a9dd
Yasirah <yasirahboyce1@gmail.com>
2021-11-15 15:27:06 +0000

    cc-15

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//write your code here
+function findLongestWord (array) { 
+
+let length = 0;
+let longest;
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i].length > length) {
+    length = array[i].length;
+    longest = array[i];
+  }
+  }
+  return longest 
+}
+
+console.log(findLongestWord(['My', 'Name', 'Is', 'yaya', 'Girllll'])) 
